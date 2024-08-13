ef5cb3f056e938631ad2b39e46f428eeade0f213
PunitChawla <chawlapunit79@gmail.com>
2024-08-13 11:28:07 +0530

    fix main page route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import { Signup } from './pages/singup'
 import { Signin } from './pages/singin'
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+    <Route path="/" element={<Navigate to= '/signup' />} />
       <Route path='/signup' element=  {< Signup />}></Route>
       <Route path='/signin' element=  {< Signin />}></Route>
       <Route path='/blog/:id' element=  {< Blog />}></Route>
